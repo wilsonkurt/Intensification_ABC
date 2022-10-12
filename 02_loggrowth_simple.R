8b46983344cae4af9ece0ae1438ea31c632324db
Simon Brewer <brewersi@gmail.com>
2022-10-12 14:07:18 -0600

    Log growth function (and simple test)

--- 02_loggrowth_simple.R
+++ 02_loggrowth_simple.R
@@ -9,3 +9,27 @@
 ## NPPlo: NPP value corresponding to K=0
 ## NPPhi: NPP value corresponding to K=Kmax
 
+library(ggplot2)
+
+log_growth <- function(N0, r, K, time) {
+  N_t <- rep(NA, length(time))
+  N_t <- N0
+  for (i in 2:length(time)) {
+    N_t[i] = K / (1 + ((K - N0) / N0) * exp(-r * time[i]))
+  }
+  return(list(time = time,
+              N = N_t))
+}
+
+## Simple example
+myN0 <- 2
+myr <- 0.05
+myK <- 500
+mytime <- seq(1, 500)
+results <- log_growth(N0 = myN0, 
+                      r = myr, 
+                      K = myK, 
+                      time = mytime)
+
+plot(results$time, results$N, 
+     type = 'l')
