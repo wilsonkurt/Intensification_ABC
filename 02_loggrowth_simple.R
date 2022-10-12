868bd7062ca866e5dfbbc6db4d651d03938d370c
Simon Brewer <brewersi@gmail.com>
2022-10-12 13:40:12 -0600

    Framework for NPP driven log growth model

---
+++ 02_loggrowth_simple.R
@@ -0,0 +1,11 @@
+## Single random rin of log growth model
+## Sample the following
+## tstart (initial settlement)
+## npp_lo: minimum NPP for growth
+## npp_hi: maximum NPP for growth
+## cK0: initial number of individuals
+## r: growth rate
+## Kmax: max carrying capacity
+## NPPlo: NPP value corresponding to K=0
+## NPPhi: NPP value corresponding to K=Kmax
+
