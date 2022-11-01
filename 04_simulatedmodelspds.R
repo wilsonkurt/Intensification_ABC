33401e41a31f00e671e63fc3682c166fe15d66f0
wilsonkurt <wilson.kurtm@gmail.com>
2022-11-01 15:33:47 -0600

    latest update
    updated code for ABC

--- 04_simulatedmodelspds.R
+++ 04_simulatedmodelspds.R
@@ -20,16 +20,24 @@ createmodelspd <- function(model) {
   }  
 
 #Now create spds from our model simulated data
-d <- uncalibrate(model, calCurves = 'intcal20', verbose = F)#uncalibrate the dates from the simulated growth model
-n.samples <- length(index)#we will sample the uncalibtrated dates equal to the unique bins
-errors <- sample(dat_cal.Plat.norm$metadata$Error, size = n.samples, replace =TRUE) #generate date errors by sampling from the observed errors of the actual dates
-uncal.samples <- sample(d$CRA, size = n.samples, prob = d$PrDens, replace = TRUE)#sample the n.samples (so # of dates) we had using that calcurve from the uncalibrated growth model output - weight the probability of selection by the PrDens of the growth model output
-cal.samples <- sample(d$CRA, size = n.samples, prob = d$Raw, replace = TRUE)#do the same thing, but instead of weighting hte probability by the PrDens, weight by the Raw output from the uncalibrate process
+d <- uncalibrate(model, calCurves = 'intcal20', verbose = F)#uncalibrate the spd from the simulated growth model
+n.samples <- length(allbins)#we will sample the uncalibtrated dates equal to the unique bins
+errors <- sample(dat_cal.Plat.norm$metadata$Error, size = n.samples, replace =TRUE) #generate date errors by sampling 
+#from the observed errors of the actual dates
+uncal.samples <- sample(d$CRA, size = n.samples, prob = d$PrDens, replace = TRUE)#sample the n.samples (so # of dates) 
+#we had using that calcurve from the uncalibrated growth model output - weight the probability of selection by the 
+#PrDens of the growth model output
+cal.samples <- sample(d$CRA, size = n.samples, prob = d$Raw, replace = TRUE)#do the same thing, but instead of weighting 
+#hte probability by the PrDens, weight by the Raw output from the uncalibrate process
 ids <- idCounter + (1:n.samples)#assign an ID (this will just be a unique number from 1 to the total # samples we generated) 
-uncalsample.norm <- calibrate(uncal.samples, errors, calCurves = 'intcal20', ids = ids, normalised = TRUE, verbose = FALSE) #calibrate a new SPD using the artificial 14c dates sampled from the growthModel output based on sampling using PrDens while  normalising the SPD
-uncalsample.nnorm <- calibrate(uncal.samples, errors, calCurves = 'intcal20', ids = ids, normalised = FALSE, verbose = FALSE) #calibrate a new SPD using the artificial 14c dates sampled from the growthModel output based on sampling using Raw while not normalising the SPD
-calsample.norm <- calibrate(cal.samples, errors, calCurves = 'intcal20', ids = ids, normalised = TRUE, verbose = FALSE)#calibrate a new SPD using the artificial 14c dates sampled from the growthModel output based on sampling using Raw while normalising the SPD
-calsample.nnorm <- calibrate(cal.samples, errors, calCurves = 'intcal20', ids = ids, normalised = FALSE, verbose = FALSE) #calibrate a new SPD using the artificial 14c dates sampled from the growthModel output based on sampling using Raw while not normalising the SPD
+uncalsample.norm <- calibrate(uncal.samples, errors, calCurves = 'intcal20', ids = ids, normalised = TRUE, verbose = FALSE) #calibrate 
+#a new SPD using the artificial 14c dates sampled from the growthModel output based on sampling using PrDens while  normalising the SPD
+uncalsample.nnorm <- calibrate(uncal.samples, errors, calCurves = 'intcal20', ids = ids, normalised = FALSE, verbose = FALSE) #calibrate 
+#a new SPD using the artificial 14c dates sampled from the growthModel output based on sampling using Raw while not normalising the SPD
+calsample.norm <- calibrate(cal.samples, errors, calCurves = 'intcal20', ids = ids, normalised = TRUE, verbose = FALSE)#calibrate a new 
+#SPD using the artificial 14c dates sampled from the growthModel output based on sampling using Raw while normalising the SPD
+calsample.nnorm <- calibrate(cal.samples, errors, calCurves = 'intcal20', ids = ids, normalised = FALSE, verbose = FALSE) #calibrate a 
+#new SPD using the artificial 14c dates sampled from the growthModel output based on sampling using Raw while not normalising the SPD
 
 #Now make an spd using the resampled dates for each of normalised, not normalised, and each version of back calculating 14c dates
 spd.uncalsample.norm <- spd(uncalsample.norm, timeRange = c(5000,0), spdnormalised = TRUE, verbose = FALSE)
